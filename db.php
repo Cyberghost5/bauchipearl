f1bd9d35dd5edc59f63753b0c0b6a62fe482eb56
Adebisi Covenant <adebisicovenant01@gmail.com>
2026-01-20 14:58:22 +0100

    First

--- db.php
+++ db.php
@@ -8,9 +8,9 @@ function db(): PDO
     }
 
     $host = getenv('DB_HOST') ?: 'localhost';
-    $name = getenv('DB_NAME') ?: 'bauchi_pearl';
-    $user = getenv('DB_USER') ?: 'root';
-    $pass = getenv('DB_PASS') ?: '';
+    $name = getenv('DB_NAME') ?: 'achabang_bauchi';
+    $user = getenv('DB_USER') ?: 'achabang_bauchi';
+    $pass = getenv('DB_PASS') ?: 'achabang_bauchi';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host={$host};dbname={$name};charset={$charset}";
